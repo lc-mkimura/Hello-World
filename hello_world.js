7e46339b655e42dc9c72bab1bdb6406d72b9b89c
lc-mkimura <kimura_m@lightcafe.co.jp>
2015-04-09 19:13:42 +0900

    Create hello_world.js

---
+++ hello_world.js
@@ -0,0 +1,25 @@
+$(function() {
+
+	/**
+	 * 初期処理
+	 */
+	var init = function() {
+
+		/** 「Hello World!!」をalertで出す */
+		test();
+
+	};
+
+	/**
+	 * 「Hello World!!」をalertで出す
+	 */
+	var test = function() {
+
+		// alert表示
+		alert('Hello World!!');
+
+	};
+
+	/** 初期処理 */
+	init();
+});
